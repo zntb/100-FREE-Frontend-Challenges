4dc61719ae92d025a6b1f2f8f5ffcff168819e75
zntb <zenetibi@yahoo.com>
2024-07-16 18:24:14 +0300

    Update email input settings

--- 10-Sign-Up-Page/index.js
+++ 10-Sign-Up-Page/index.js
@@ -19,3 +19,22 @@ getPasswordBtn.addEventListener("click", () => {
   passwordInput.type = isPasswordVisible ? "password" : "text";
   isPasswordVisible = !isPasswordVisible;
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const emailInput = document.getElementById("email");
+
+  emailInput.addEventListener("input", function () {
+    validateEmail(emailInput);
+  });
+
+  function validateEmail(input) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (input.value === "") {
+      input.removeAttribute("data-invalid");
+    } else if (!emailPattern.test(input.value)) {
+      input.setAttribute("data-invalid", "true");
+    } else {
+      input.removeAttribute("data-invalid");
+    }
+  }
+});
